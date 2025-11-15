b8c1757dd7682ca12bdc35886cf059e2b29faf19
Alessandro Lo Verde <alessandro.loverde@gmail.com>
2025-11-15 19:59:48 +0100

    ★ Logged User as a composable

---
+++ app/components/LoggedUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="loggedUser" class="logged-user"><span>Loggein as: </span>{{ loggedUser.name }}</div>
+</template>
+
+<script setup lang="ts">
+
+const { participant: loggedUser } = useLoggedUser()
+
+</script>
+
+<style scoped>
+
+.logged-user {
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #000;
+  background-color: #f0f0f0;
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
+
--- app/pages/teams/[id].vue
+++ app/pages/teams/[id].vue
@@ -1,4 +1,5 @@
 <template>
+   <LoggedUser></LoggedUser>
    <div class="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <nuxtLink to="/">Back to Home</nuxtLink>
       <h1>{{ teamData?.name }}</h1>
